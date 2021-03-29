706f06fdc3e705072f42b0b3a6187d9ce162d545
Igor Ivanov <igor.dev.ivanov@gmail.com>
2021-03-29 23:57:33 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import List from "./data";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
+const App = () => {
+  const list = List.list;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext
+          onDragEnd={(params) => {
+          const srcIndex = params.source.index;
+          const desIndex = params.destination.index;
+          list.splice(desIndex, 0, list.splice(srcIndex, 1)[0]);
+        }}
+      >
+        <div className="main-box">
+          <Droppable droppableId="droppable-1">
+            {(provided, _) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {list.map((item, i) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={`draggable-${item.id}`}
+                    index={i}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        className="slide-boxes"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={{
+                          ...provided.draggableProps.style,
+                          border: snapshot.isDragging
+                            ? "3px solid red"
+                            : "3px solid gray",
+                        }}
+                      >
+                        {item.title}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const List = {
+  list: [
+    {
+      id: 1,
+      title: "FIRST SLIDE",
+    },
+    {
+      id: 2,
+      title: "SECOND SLIDE",
+    },
+    {
+      id: 3,
+      title: "THIRD SLIDE",
+    }
+   
+  ],
+  // getList: function () {
+  //   return (
+  //     (localStorage.getItem("theList") &&
+  //       JSON.parse(localStorage.getItem("theList"))) ||
+  //     this.list
+  //   );
+  // },
+  // saveList: (list) => {
+  //   localStorage.setItem("theList", JSON.stringify(list));
+  // },
+};
+
+export default List;
